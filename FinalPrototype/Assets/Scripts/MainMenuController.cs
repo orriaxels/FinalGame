8009a4216a8d901c3a2deaf25394de655f9a5716
haraldurshoshan <haraldurshoshan@gmail.com>
2018-04-01 16:23:46 +0000

    main menu stuff

---
+++ FinalPrototype/Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Rewired;
+using UnityEngine.EventSystems;
+
+public class MainMenuController : MonoBehaviour {
+
+	public int playerId = 0; // The Rewired player id of this character
+	private Player player; // The Rewired Player
+	public EventSystem eventSystem;
+	public GameObject selectedObject;
+	public GameObject backButton;
+
+	private bool buttonSelected;
+
+	int motorIndex = 0; // the first motor
+	float motorLevel = 0.1f; // full motor speed
+	float duration = 0.1f;
+	// Use this for initialization
+	void Start () {
+		player = ReInput.players.GetPlayer(playerId);
+	}
+	
+	// Update is called once per frame
+    void Update () 
+    {
+        if (player.GetAxis ("UIVertical") != 0) 
+        {
+			player.SetVibration(motorIndex, motorLevel, duration);
+        }
+
+		if(player.GetButtonDown("UISubmit"))
+		{
+			Debug.Log("submit, should vibrate");
+			player.SetVibration(motorIndex, motorLevel, duration);
+		}
+    }
+}
---
+++ FinalPrototype/Assets/Scripts/QuitOnClick.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitOnClick : MonoBehaviour {
+
+	public void Quit()
+	{
+		#if UNITY_EDITOR
+			// Unity_EDITOR.EditorApplication.isPlaying = false;
+			// UnityEditor.EditorApplication.isPlaying = false;
+			UnityEditor.EditorApplication.isPlaying = false;
+		#else
+			Application.Quit();
+		#endif
+	}
+}
